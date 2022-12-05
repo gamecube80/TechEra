24cc188119c3537c54e7e0ab423f0502f787c98b
gamecube80 <gamecube80@gmail.com>
2022-12-05 08:21:39 -0600

    Created AngularAjax

--- childComponent/src/app/app.component.ts
+++ childComponent/src/app/app.component.ts
@@ -8,11 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Child Components';
 
-  firstname: string = "";
+  // firstname: string = "";
 
-  onChangeEventFirst(event: any) {
-
-    this.firstname = (event.target.value);
-
+  onSubmit(data: any) {
+    console.log(data.value);
   }
+
+  // onChangeEventFirst(event: any) {
+
+  //   this.firstname = (event.target.value);
+
+  // }
 }
